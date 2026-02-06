4b064ff76c33fd05226257d5f37006b4b966c797
Santiago Erazo <serazo@usfq.edu.ec>
2026-02-05 19:35:02 -0500

    Primer cambio

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -5,17 +5,17 @@
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
           <ion-icon aria-hidden="true" :icon="triangle" />
-          <ion-label>Primero</ion-label>
+          <ion-label>First tab</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab2" href="/tabs/tab2">
           <ion-icon aria-hidden="true" :icon="ellipse" />
-          <ion-label>Segundo</ion-label>
+          <ion-label>Second tab</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab3" href="/tabs/tab3">
           <ion-icon aria-hidden="true" :icon="square" />
-          <ion-label>Tercero</ion-label>
+          <ion-label>Third tab</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
